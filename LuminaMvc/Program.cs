f3615eb4baae625d83de86701abacdec5570f8f5
jemslzr <salazarclarissejem@gmail.com>
2026-01-13 11:42:54 +0800

    Act 4: Fixed database creation

--- LuminaMvc/Program.cs
+++ LuminaMvc/Program.cs
@@ -5,9 +5,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("LuminaDb")); 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// --- AUTO-CREATE DATABASE  ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<LuminaMvc.Data.AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
